f6e2151a5c9cda872c206330060324b87b3e48c5
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-01-20 12:19:00 +0900

    [add] Scenarioの読み込みを実装

--- Bve5ScenarioEditor/MainWindow.xaml.cs
+++ Bve5ScenarioEditor/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bve5ScenarioEditor
 {
@@ -20,9 +11,35 @@ namespace Bve5ScenarioEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string dirPath = @"F:\Library\Documents\Bvets\Scenarios"; //TODO
+
+        /// <summary>
+        /// 現在のディレクトリにあるシナリオファイルを読み込みます。
+        /// </summary>
+        void LoadScenarios()
+        {
+
+            //とりあえずパスの追加 TODO: あとで修正
+            filePathComboBox.Items.Add(dirPath);
+            this.filePathComboBox.SelectedIndex = this.filePathComboBox.Items.Count - 1;
+
+            //シナリオの読み込み
+            string[] files = Directory.GetFiles(dirPath, "*");
+            foreach(string file in files)
+            {
+                Scenario scenario = new Scenario(file);
+                if (scenario.Load())
+                {
+                    scenarioSelectListView = scenario.AddListViewItem(scenarioSelectListView);
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadScenarios();
         }
     }
 }
---
+++ Bve5ScenarioEditor/Scenario.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Bve5_Parsing.ScenarioGrammar;
+using Hnx8.ReadJEnc;
+
+namespace Bve5ScenarioEditor
+{
+    class Scenario
+    {
+        public FileInfo File { get; private set; }
+        public ScenarioData Data { get; set; }
+
+        /// <summary>
+        /// ファイルパスを指定してシナリオを作成します。
+        /// </summary>
+        /// <param name="path">シナリオファイルのパス</param>
+        public Scenario(string path)
+        {
+            File = new FileInfo(path);
+        }
+
+        /// <summary>
+        /// シナリオファイルを読み込んで解析します。
+        /// </summary>
+        /// <returns>シナリオが正常に読み込めたかどうか</returns>
+        public bool Load()
+        {
+            //ReadJEncを利用してファイルの読み込み
+            using (Hnx8.ReadJEnc.FileReader reader = new FileReader(this.File))
+            {
+                Hnx8.ReadJEnc.CharCode c = reader.Read(this.File);
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("Loading ScenarioFile【{0}】...", this.File.Name);
+                string text = reader.Text;
+                if (c is CharCode.Text)
+                {
+                    //読み込んだファイルがテキスト
+                    Console.WriteLine("Encoding: {0}", c.Name);
+                    ScenarioGrammarParser parser = new ScenarioGrammarParser();
+
+                    try
+                    {
+                        this.Data = parser.Parse(text);
+                        Console.WriteLine("Parse Successful !");
+                        return true;
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.Error.WriteLine("Scenario: 【{0}】header mismatched.", this.File.FullName);
+                    }
+                }
+                else
+                {
+                    //テキストファイルではない
+                    Console.Error.WriteLine("【{0}】is not text file.", this.File.FullName);
+                }
+            }
+            Console.Error.WriteLine("Scenario Load Error");
+            return false;
+        }
+
+        /// <summary>
+        /// シナリオをアイテムに追加したリストビューを返します。
+        /// </summary>
+        /// <param name="listView">追加する対象のリストビュー</param>
+        /// <returns>シナリオを追加したリストビュー</returns>
+        public ListView AddListViewItem(ListView listView)
+        {
+            ListViewItem item = new ListViewItem(Data.Title);
+
+            listView.Items.Add(item);
+            return listView;
+        }
+    }
+}
